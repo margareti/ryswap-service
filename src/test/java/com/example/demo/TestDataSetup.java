e559e10c8c2763c2920b6b5e8fd7b8a46130b561
Penio.ivanov@mastercard.com <Penio.ivanov@mastercard.com>
2018-10-18 17:47:40 +0100

    fixed persisting of flight route time in test.

--- src/test/java/com/example/demo/TestDataSetup.java
+++ src/test/java/com/example/demo/TestDataSetup.java
@@ -2,17 +2,16 @@ package com.example.demo;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import com.example.demo.flights.Flight;
 import com.example.demo.flights.airport.Airport;
 import com.example.demo.flights.airport.AirportRepository;
 import com.example.demo.flights.route.FlightRoute;
 import com.example.demo.flights.route.FlightRouteRepository;
 import com.example.demo.flights.route.FlightRouteTime;
+import com.example.demo.flights.route.FlightRouteTimeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -23,6 +22,9 @@ public final class TestDataSetup {
     private AirportRepository airportRepository;
     @Autowired
     private FlightRouteRepository flightRouteRepository;
+
+    @Autowired
+    private FlightRouteTimeRepository flightRouteTimeRepository;
 
     public void setUpAirports() {
         createAirport("BCN", "Barcelona International Airport");
@@ -43,10 +45,10 @@ public final class TestDataSetup {
 
     public void setUpRoutes() {
         FlightRoute fr = createFligthRoute("SOF", "EDI");
-        List<FlightRouteTime> flightRouteTimes = createTimes(DayOfWeek.MONDAY, LocalTime.of(12, 0), LocalTime.of(21, 30) );
-        flightRouteTimes.addAll(createTimes(DayOfWeek.FRIDAY, LocalTime.of(13, 30), LocalTime.of(18, 30) ));
+        List<FlightRouteTime> flightRouteTimes = createTimes(fr, DayOfWeek.MONDAY, LocalTime.of(12, 0), LocalTime.of(21, 30) );
+        flightRouteTimes.addAll(createTimes(fr, DayOfWeek.MONDAY, LocalTime.of(13, 30), LocalTime.of(18, 30) ));
         fr.setFlightRouteTimes(flightRouteTimes);
-        flightRouteRepository.saveAndFlush(fr);
+        flightRouteRepository.save(fr);
         System.out.println(fr);
     }
 
@@ -57,9 +59,9 @@ public final class TestDataSetup {
         return flightRouteRepository.save(fr);
     }
 
-    private List<FlightRouteTime> createTimes(DayOfWeek dayOfWeek, LocalTime ...times) {
+    private List<FlightRouteTime> createTimes(FlightRoute fr, DayOfWeek dayOfWeek, LocalTime... times) {
         return Stream.of(times).map(t -> {FlightRouteTime frt = new FlightRouteTime();
-        frt.setDayOfWeek(dayOfWeek); frt.setTime(t); return frt;}).collect(Collectors.toList());
+        frt.setDayOfWeek(dayOfWeek); frt.setTime(t); frt.setFlightRoute(fr); return frt;}).map(frt -> flightRouteTimeRepository.save(frt) ).collect(Collectors.toList());
     }
 
 
